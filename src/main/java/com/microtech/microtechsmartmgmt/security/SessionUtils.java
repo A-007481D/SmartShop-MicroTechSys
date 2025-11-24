b055f71a2c5bd416fefb63527d6e6371d7a17bac
A-007481D <labidabdelmalek@gmail.com>
2025-11-24 22:26:31 +0100

    feat: create a helper for session related operations

---
+++ src/main/java/com/microtech/microtechsmartmgmt/security/SessionUtils.java
@@ -0,0 +1,22 @@
+package com.microtech.microtechsmartmgmt.security;
+
+import com.microtech.microtechsmartmgmt.entity.User;
+import jakarta.servlet.http.HttpSession;
+
+import static com.microtech.microtechsmartmgmt.security.SessionUtils.getUser;
+
+public class SessionUtils {
+    public static final String SESSION_USER_KEY = "CURRENT_USER";
+
+    public static void setUser(HttpSession session, User user) {
+        session.setAttribute(SESSION_USER_KEY, user);
+    }
+
+    public static User getUser(HttpSession session) {
+        return (User) session.getAttribute(SESSION_USER_KEY);
+    }
+
+    public static boolean isLoggedIn(HttpSession session) {
+        return getUser(session) != null;
+    }
+}
