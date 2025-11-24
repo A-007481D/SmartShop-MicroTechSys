9fe039899134518c59ef4debaefae2e386b31cf2
A-007481D <labidabdelmalek@gmail.com>
2025-11-24 22:41:42 +0100

    feat: create a configration file to register the auth interceptor

---
+++ src/main/java/com/microtech/microtechsmartmgmt/config/WebConfig.java
@@ -0,0 +1,22 @@
+package com.microtech.microtechsmartmgmt.config;
+
+import com.microtech.microtechsmartmgmt.security.AuthInterceptor;
+import lombok.RequiredArgsConstructor;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+@RequiredArgsConstructor
+public class WebConfig implements WebMvcConfigurer {
+
+    private final AuthInterceptor authInterceptor;
+
+    @Override
+    public void addInterceptors(InterceptorRegistry registry) {
+        registry.addInterceptor(authInterceptor)
+                .addPathPatterns("/api/**")
+                .excludePathPatterns("/api/auth/**", "/swagger-ui/**", "/v3/api-docs/**");
+    }
+}
