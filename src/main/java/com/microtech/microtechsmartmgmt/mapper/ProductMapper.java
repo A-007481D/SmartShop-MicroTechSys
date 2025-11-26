0d5db26e7f07fa1ea7c1a7063d8b6734808237c5
A-007481D <labidabdelmalek@gmail.com>
2025-11-26 22:22:14 +0100

    feat(product): add ProductMapper - toResponse: Product -> ProductResponse

---
+++ src/main/java/com/microtech/microtechsmartmgmt/mapper/ProductMapper.java
@@ -0,0 +1,13 @@
+package com.microtech.microtechsmartmgmt.mapper;
+
+import com.microtech.microtechsmartmgmt.dto.response.ProductResponse;
+import com.microtech.microtechsmartmgmt.entity.Product;
+import org.mapstruct.Mapper;
+import org.mapstruct.MappingConstants;
+
+@Mapper(componentModel = MappingConstants.ComponentModel.SPRING)
+public interface ProductMapper {
+
+    ProductResponse toResponse(Product product);
+}
+
